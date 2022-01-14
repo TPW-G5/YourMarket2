6c7d1d6344f13bd20679a9f81eadcca346067e9d
daniff15 <dani.fig@ua.pt>
2022-01-14 00:27:40 +0000

    footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './client/footer/footer.component';
 import { NavBarComponent } from './client/nav-bar/nav-bar.component';
 
 @NgModule({
